1d76e9eb35f9045687444cee360cf4688a4437dc
InteractiveNinja <gabrielnadolny2@gmail.com>
2021-08-20 14:06:03 +0200

    new bootstrap imports, main design + bootstrap icons

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-alert("Hey :D");
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "./style.css";
+// import { fromEvent } from "rxjs";
+//
+// fromEvent(document, "click").subscribe(console.log);
