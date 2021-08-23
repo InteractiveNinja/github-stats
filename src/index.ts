a8061340a63c77f6b464b3cf1346ba3eb01f86f6
InteractiveNinja <gabrielnadolny2@gmail.com>
2021-08-23 09:40:51 +0200

    Added repo support and error catching

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,20 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "./style.css";
-import { debounceTime, fromEvent, switchMap } from "rxjs";
+import {
+  catchError,
+  debounceTime,
+  empty,
+  filter,
+  forkJoin,
+  fromEvent,
+  map,
+  mapTo,
+  of,
+  switchMap,
+  tap,
+  throwError,
+} from "rxjs";
 import $ from "jquery";
 import { ajax } from "rxjs/ajax";
 
@@ -18,36 +31,40 @@ type USER_API = {
   twitter_username: string;
 };
 
-const createCard = ({
-  login,
-  name,
-  bio,
-  followers,
-  location,
-  avatar_url,
-  following,
-  public_repos,
-  twitter_username,
-}: USER_API) =>
-  // USER_REPOS: REPO_API
+const createCard = (
   {
-    clearFields();
-    $("#git-card").show();
-    !name ? $("#git-name").text(login) : $("#git-name").text(name);
-    if (bio) $("#git-bio").text(bio);
-    if (followers) $("#git-follower").text(followers);
-    if (location) $("#git-location").text(location);
-    if (avatar_url) $("#git-pic").attr("src", avatar_url);
-    if (following) $("#git-follows").text(following);
-    if (public_repos) $("#git-repos").text(public_repos);
-    if (twitter_username) $("#git-twitter").text(`@${twitter_username}`);
+    login,
+    name,
+    bio,
+    followers,
+    location,
+    avatar_url,
+    following,
+    public_repos,
+    twitter_username,
+  }: USER_API,
+  USER_REPOS: REPO_API[]
+) => {
+  clearFields();
+  $("#git-card").show();
+  !name ? $("#git-name").text(login) : $("#git-name").text(name);
+  if (bio) $("#git-bio").text(bio);
+  if (followers) $("#git-follower").text(followers);
+  if (location) $("#git-location").text(location);
+  if (avatar_url) $("#git-pic").attr("src", avatar_url);
+  if (following) $("#git-follows").text(following);
+  if (public_repos) $("#git-repos").text(public_repos);
+  if (twitter_username) $("#git-twitter").text(`@${twitter_username}`);
 
-    //Wird erst gebraucht wenn Repos supportet werden
-    // for (let i = 0; i < 5; i++) {
-    //     let html = createRepoCard(USER_REPOS[i]);
-    //     $("#git-repos-list").append(html);
-    // }
-  };
+  if (USER_REPOS.length >= 3) {
+    for (let i = 0; i < 3; i++) {
+      const entry = USER_REPOS[i];
+      if (!entry.url || !entry.name || !entry.language) continue;
+      const html = createRepoCard(USER_REPOS[i]);
+      $("#git-repos-list").append(html);
+    }
+  }
+};
 
 const clearFields = () => {
   $("#git-name").text("");
@@ -58,10 +75,10 @@ const clearFields = () => {
   $("#git-follows").text("");
   $("#git-repos").text("");
   $("#git-twitter").text("");
+  $("#git-repos-list").html("");
 };
 
 const createRepoCard = ({ language, name, url }: REPO_API) => {
-  if (!language || !name || !url) return "";
   return ` <div class="border border-2 rounded rounded-2 p-3 m-2 text-center">
                         <p class="fs-4">${name}</p>
                         <p class="fs-4">${language}</p>
@@ -73,9 +90,25 @@ const input$ = fromEvent($("#username"), "input");
 input$
   .pipe(
     debounceTime(1000),
-    switchMap((event: any) => {
-      const term = event.target.value;
-      return ajax.getJSON(`https://api.github.com/users/${term}`);
+    map((m) => (<HTMLInputElement>m.target).value),
+    filter((f) => f != ""),
+    switchMap((name) => {
+      return forkJoin({
+        user: ajax.getJSON(`https://api.github.com/users/${name}`).pipe(
+          catchError((error) => {
+            return of("error");
+          })
+        ),
+        repo: ajax.getJSON(`https://api.github.com/users/${name}/repos`).pipe(
+          catchError((error) => {
+            return of("error");
+          })
+        ),
+      });
     })
   )
-  .subscribe((val) => createCard(<USER_API>val));
+  .subscribe((val) => {
+    if (val.repo == "error" || val.user == "error")
+      return alert("User nicht gefunden");
+    createCard(<USER_API>val.user, <REPO_API[]>val.repo);
+  });
